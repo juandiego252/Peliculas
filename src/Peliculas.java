a3a9ee90fa9b9d8905ca411aeada168a86f83824
Juan Diego <juandiegojd252@gmail.com>
2023-05-30 09:26:58 -0500

    Agregado Atributos, Constructor setters y getters

--- src/Peliculas.java
+++ src/Peliculas.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Peliculas {
+public class Peliculas {
+
+    int Duracion;
+    String Nombre;
+    String tipo;
+
+    public Peliculas(int duracion, String nombre, String tipo) {
+        Duracion = duracion;
+        Nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public int getDuracion() {
+        return Duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        Duracion = duracion;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
