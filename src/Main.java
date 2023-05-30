cf35245bdc0e82bdff47ab2a2650df09073c8a62
Juan Diego <juandiegojd252@gmail.com>
2023-05-30 09:33:08 -0500

    Agregado 4 peliculas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Peliculas Titanic = new Peliculas(2,"Titanic","Drama");
+        Peliculas Titanic = new Peliculas(2,"Titanic","Drama"); // Juan
+        Peliculas fast_Furios = new Peliculas(2,"Rapidos Y furiosos","Accion"); // Juan
+        Peliculas After = new Peliculas(2,"After","Romance"); // Juan
+        Peliculas Sherk = new Peliculas(2,"Sherk","Animada"); // Juan
+
     }
 }
