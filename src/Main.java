20168f03213f7fa3b748aa00bb0b902184a5ab01
Juan Falconi <juan.falconi@epn.edu.ec>
2023-05-30 09:44:38 -0500

    Chicos agregue clasificacion

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        Peliculas Titanic = new Peliculas(2,"Titanic","Drama"); // Juan
-        Peliculas fast_Furios = new Peliculas(2,"Rapidos Y furiosos","Accion"); // Juan
-        Peliculas After = new Peliculas(2,"After","Romance"); // Juan
-        Peliculas Sherk = new Peliculas(2,"Sherk","Animada"); // Juan
-        Peliculas colorouts = new Peliculas(1,"El color que cayo del cielo","Terror"); //Heyer
-        Peliculas Kingsman = new Peliculas(2,"Kingsman, el servicio secreto","Accion");//Heyer
-        Peliculas Endgame = new Peliculas(3,"Avengers: Endgame","Accion");//Heyer
-        Peliculas chihiro = new Peliculas(2,"El viaje de Chihiro","Anime");//Heyer
+        Peliculas Titanic = new Peliculas(2,"Titanic","Drama","Mayores de 12 años"); // Juan
+        Peliculas fast_Furios = new Peliculas(2,"Rapidos Y furiosos","Accion","Mayores de 13 años"); // Juan
+        Peliculas After = new Peliculas(2,"After","Romance","Mayores de 18 años"); // Juan
+        Peliculas Sherk = new Peliculas(2,"Sherk","Animada","Todo publico"); // Juan
+        Peliculas colorouts = new Peliculas(1,"El color que cayo del cielo","Terror","Mayor de 18 años"); //Heyer
+        Peliculas Kingsman = new Peliculas(2,"Kingsman, el servicio secreto","Accion","Mayores de 12 años");//Heyer
+        Peliculas Endgame = new Peliculas(3,"Avengers: Endgame","Accion","Mayores de 12 años");//Heyer
+        Peliculas chihiro = new Peliculas(2,"El viaje de Chihiro","Anime","Mayores de 7 años");//Heyer
+        Peliculas Toy_Story = new Peliculas(1,"Toy Story","Animada","Todo Publico");
+        Peliculas Avatar = new Peliculas(2,"Avatar","Ciencia Ficcion","Mayores de 13 años");
+        Peliculas CJ7 = new Peliculas(1,"CJ7","Ciencia Ficcion-Comedia","Mayores de 7 años");
+        Peliculas Spiderman = new Peliculas(2,"El hombre araña","Fantasia","mayores de 12 años");
 
     }
 }
