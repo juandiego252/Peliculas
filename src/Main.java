848bfc8627532949ea70fd759a8fa749fa8c4d13
Orland H <24heyerunsu@gmail.com>
2023-05-30 09:35:29 -0500

    Peliculas Orland

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         Peliculas fast_Furios = new Peliculas(2,"Rapidos Y furiosos","Accion"); // Juan
         Peliculas After = new Peliculas(2,"After","Romance"); // Juan
         Peliculas Sherk = new Peliculas(2,"Sherk","Animada"); // Juan
+        Peliculas colorouts = new Peliculas(1,"El color que cayo del cielo","Terror"); //Heyer
+        Peliculas Kingsman = new Peliculas(2,"Kingsman, el servicio secreto","Accion");//Heyer
+        Peliculas Endgame = new Peliculas(3,"Avengers: Endgame","Accion");//Heyer
+        Peliculas chihiro = new Peliculas(2,"El viaje de Chihiro","Anime");//Heyer
 
     }
 }
